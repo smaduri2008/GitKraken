048693fb07b94533b47672f9b12e982513dc308d
sahas <sahas.maduri@gmail.com>
2025-11-25 13:38:22 -0500

    created executor service implementation

---
+++ ExecutorServiceCaller.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorServiceCaller{
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(1000);
+
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+
+            Callable<Integer> task = () -> {
+                int num = 0;
+                for (int j = 0; j < 1_000_000; j++) {
+                    num += 1;
+                }
+                return num;
+            };
+
+            futures.add(executor.submit(task));
+        }
+
+       
+		
+
+    }
+}
